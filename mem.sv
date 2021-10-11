29b956ac8d7a0f2e5afae045f6763da1f423f61d
Bratan <alfabrayan12@gmail.com>
2021-10-11 01:26:15 -0600

    Bug fix de memoria

--- mem.sv
+++ mem.sv
@@ -13,7 +13,7 @@
 	- rd2: data requested
 */
 
-module mem #(parameter WIDTH = 16, parameter INSTRUCTIONWIDTH = 24, parameter RAMSIZE = 4096)
+module mem #(parameter WIDTH = 16, parameter INSTRUCTIONWIDTH = 24, parameter RAMSIZE = 4)
   (input logic clk, we,
 	input logic [WIDTH-1:0] a1, a2, wd,
 	output logic [INSTRUCTIONWIDTH-1:0] rd1,
@@ -26,7 +26,7 @@ module mem #(parameter WIDTH = 16, parameter INSTRUCTIONWIDTH = 24, parameter RA
 	assign weAux[2:0] = 0;
 	assign wdAux[WIDTH*3-1:0] = 0;
 	assign aAux[WIDTH*3-1:0] = {a1, a1, a1};
-	assign rd1 = rdAux[8*3-1:0];
+	assign rd1 = {rdAux[8-1:0], rdAux[8*2-1:8], rdAux[8*3-1:8*2]};
 	
 	assign wdAux[WIDTH*6-1:WIDTH*3] = {wd, wd, wd};
 	assign aAux[WIDTH*4-1:WIDTH*3] = a2;
@@ -38,21 +38,22 @@ module mem #(parameter WIDTH = 16, parameter INSTRUCTIONWIDTH = 24, parameter RA
 														 aAux, wdAux,
 														 rdAux);
 	
-	always_ff @(we) begin
+	always_comb begin
+	
+		rd2[7:0] = 0;
+		if(a2<RAMSIZE) rd2[7:0] <= rdAux[8*4-1:8*3];
+		else if (a2<RAMSIZE*2) rd2[7:0]  <= rdAux[8*5-1:8*4];
+		else if (a2<RAMSIZE*3)  rd2[7:0]  <= rdAux[8*6-1:8*5];
+		
+	
 		weAux[5:3] = 0;
 		if (we) begin
 			$display ($sformatf("write enable a2: %h", a2));
 			if(a2<RAMSIZE) weAux[3] = 1;
 			else if (a2<RAMSIZE*2) weAux[4] = 1;
 			else if (a2<RAMSIZE*3) weAux[5] = 1;
-			
 		end
+		
 	end
 	
-	always @(a2) begin 
-		rd2[7:0] = 0;
-		if(a2<RAMSIZE) rd2[7:0] <= rdAux[8*4-1:8*3];
-		else if (a2>=RAMSIZE && a2<RAMSIZE*2) rd2[7:0]  <= rdAux[8*5-1:8*4];
-		else if (a2>=RAMSIZE*2 && a2<RAMSIZE*3)  rd2[7:0]  <= rdAux[8*6-1:8*5];
-	end
 endmodule
