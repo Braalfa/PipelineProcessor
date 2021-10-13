6b8a745b19440be8d341b11782fb1cdd09f88994
Bratan <alfabrayan12@gmail.com>
2021-10-13 00:38:00 -0600

    se elimina el registro del pc

--- regfile.sv
+++ regfile.sv
@@ -17,7 +17,7 @@ module regfile #(parameter WIDTH = 16, parameter REGNUM = 16, parameter ADDRESSW
 	(input logic clk,
 	input logic we3,
 	input logic [ADDRESSWIDTH-1:0] ra1, ra2, wa3,
-	input logic [WIDTH-1:0] wd3, pc,
+	input logic [WIDTH-1:0] wd3,
 	output logic [WIDTH-1:0] rd1, rd2);
 	
 	logic [WIDTH-1:0] rf[REGNUM-1:0];
@@ -25,7 +25,7 @@ module regfile #(parameter WIDTH = 16, parameter REGNUM = 16, parameter ADDRESSW
 	
 	always_ff @(posedge clk)
 		if (we3) rf[wa3] <= wd3;
-		assign rd1 = (ra1 == 4'b1111) ? pc : rf[ra1];
-		assign rd2 = (ra2 == 4'b1111) ? pc : rf[ra2];
+		assign rd1 = rf[ra1];
+		assign rd2 = rf[ra2];
 
 endmodule
