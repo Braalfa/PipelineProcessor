f61a82327aae9e30cbab751fd26c2d088470b9e7
Bratan <alfabrayan12@gmail.com>
2021-10-12 23:22:16 -0600

    mem 32 bits

--- mem.sv
+++ mem.sv
@@ -13,45 +13,53 @@
 	- rd2: data requested
 */
 
-module mem #(parameter WIDTH = 16, parameter INSTRUCTIONWIDTH = 24, parameter RAMSIZE = 16)
+module mem #(parameter WIDTH = 32, parameter INSTRUCTIONWIDTH = 16, parameter RAMSIZE = 16)
   (input logic clk, we,
 	input logic [WIDTH-1:0] a1, a2, wd,
 	output logic [INSTRUCTIONWIDTH-1:0] rd1,
 	output logic [WIDTH-1:0] rd2);
 	
-	logic [5:0] weAux;
-	logic [WIDTH*6-1:0] aAux, wdAux;
-	logic [8*6-1:0] rdAux;
+	logic [6:0] weAux;
+	logic [WIDTH*7-1:0] aAux, wdAux;
+	logic [WIDTH*7-1:0] rdAux;
 	
-	assign weAux[2:0] = 0;
-	assign wdAux[WIDTH*3-1:0] = 0;
-	assign aAux[WIDTH*3-1:0] = {a1, a1, a1};
-	assign rd1 = {rdAux[8-1:0], rdAux[8*2-1:8], rdAux[8*3-1:8*2]};
+	assign weAux[0] = 0;
+	assign wdAux[WIDTH-1:0] = 0;
+	assign aAux[WIDTH-1:0] = a1;
+	assign rd1 = rdAux[WIDTH-1:0];
 	
-	assign wdAux[WIDTH*6-1:WIDTH*3] = {wd, wd, wd};
-	assign aAux[WIDTH*4-1:WIDTH*3] = a2;
-	assign aAux[WIDTH*5-1:WIDTH*4] = a2-RAMSIZE;
-	assign aAux[WIDTH*6-1:WIDTH*5] = a2-RAMSIZE*2;
-	assign rd2[15:8] = 0;
+	assign wdAux[WIDTH*7-1:WIDTH] = {wd, wd, wd, wd, wd, wd};
+	assign aAux[WIDTH*2-1:WIDTH*1] = a2;
+	assign aAux[WIDTH*3-1:WIDTH*2] = a2-RAMSIZE*1;
+	assign aAux[WIDTH*4-1:WIDTH*3] = a2-RAMSIZE*2;
+	assign aAux[WIDTH*5-1:WIDTH*4] = a2-RAMSIZE*3;
+	assign aAux[WIDTH*6-1:WIDTH*5] = a2-RAMSIZE*4;
+	assign aAux[WIDTH*7-1:WIDTH*6] = a2-RAMSIZE*5;
+
+	
 	unifiedmem #(WIDTH, RAMSIZE) unifiedmem(clk,
 														 weAux,
 														 aAux, wdAux,
 														 rdAux);
 	
 	always_comb begin
-	
-		rd2[7:0] = 0;
-		if(a2<RAMSIZE) rd2[7:0] <= rdAux[8*4-1:8*3];
-		else if (a2<RAMSIZE*2) rd2[7:0]  <= rdAux[8*5-1:8*4];
-		else if (a2<RAMSIZE*3)  rd2[7:0]  <= rdAux[8*6-1:8*5];
+		rd2 = 0;
+		if(a2<RAMSIZE) rd2 = rdAux[WIDTH*2-1:WIDTH];
+		else if (a2<RAMSIZE*2) rd2  = rdAux[WIDTH*3-1:WIDTH*2];
+		else if (a2<RAMSIZE*3)  rd2  = rdAux[WIDTH*4-1:WIDTH*3];
+		else if (a2<RAMSIZE*4)  rd2  = rdAux[WIDTH*5-1:WIDTH*4];
+		else if (a2<RAMSIZE*5)  rd2  = rdAux[WIDTH*6-1:WIDTH*5];
+		else if (a2<RAMSIZE*6)  rd2  = rdAux[WIDTH*7-1:WIDTH*6];
 		
 	
-		weAux[5:3] = 0;
+		weAux[6:1] = 0;
 		if (we) begin
-			$display ($sformatf("write enable a2: %h", a2));
-			if(a2<RAMSIZE) weAux[3] = 1;
-			else if (a2<RAMSIZE*2) weAux[4] = 1;
-			else if (a2<RAMSIZE*3) weAux[5] = 1;
+			if(a2<RAMSIZE) weAux[1] = 1;
+			else if (a2<RAMSIZE*2) weAux[2] = 1;
+			else if (a2<RAMSIZE*3) weAux[3] = 1;
+			else if (a2<RAMSIZE*4) weAux[4] = 1;
+			else if (a2<RAMSIZE*5) weAux[5] = 1;
+			else if (a2<RAMSIZE*6) weAux[6] = 1;
 		end
 		
 	end
--- unifiedmem.sv
+++ unifiedmem.sv
@@ -13,19 +13,20 @@
 	- rd2: data requested
 */
 
-module unifiedmem #(parameter WIDTH = 16, parameter RAMSIZE = 4)
+module unifiedmem #(parameter WIDTH = 32, parameter RAMSIZE = 512)
   (input logic clk,
-	input logic [5:0] we,
-	input logic [WIDTH*6-1:0] a, wd,
-	output logic [8*6-1:0] rd);
+	input logic [6:0] we,
+	input logic [WIDTH*7-1:0] a, wd,
+	output logic [WIDTH*7-1:0] rd);
 	
 
-	logic [7:0] RAM1 [RAMSIZE-1:0];
-	logic [7:0] RAM2 [RAMSIZE-1:0];
-	logic [7:0] RAM3 [RAMSIZE-1:0];
-	logic [7:0] RAM4 [RAMSIZE-1:0];
-	logic [7:0] RAM5 [RAMSIZE-1:0];
-	logic [7:0] RAM6 [RAMSIZE-1:0];
+	logic [WIDTH-1:0] RAM1 [RAMSIZE-1:0];
+	logic [WIDTH-1:0] RAM2 [RAMSIZE-1:0];
+	logic [WIDTH-1:0] RAM3 [RAMSIZE-1:0];
+	logic [WIDTH-1:0] RAM4 [RAMSIZE-1:0];
+	logic [WIDTH-1:0] RAM5 [RAMSIZE-1:0];
+	logic [WIDTH-1:0] RAM6 [RAMSIZE-1:0];
+	logic [WIDTH-1:0] RAM7 [RAMSIZE-1:0];
 
 	initial begin
 		$readmemb("C:\\Users\\alfab\\Documents\\PipelineProcessor\\mem1.txt",RAM1);
@@ -34,22 +35,24 @@ module unifiedmem #(parameter WIDTH = 16, parameter RAMSIZE = 4)
 		$readmemh("C:\\Users\\alfab\\Documents\\PipelineProcessor\\mem4.txt",RAM4);
 		$readmemh("C:\\Users\\alfab\\Documents\\PipelineProcessor\\mem5.txt",RAM5);
 		$readmemh("C:\\Users\\alfab\\Documents\\PipelineProcessor\\mem6.txt",RAM6);
+   	$readmemh("C:\\Users\\alfab\\Documents\\PipelineProcessor\\mem7.txt",RAM7);
 	end
 	
-	assign rd[8-1:0] = RAM1[a[WIDTH-1:0]]; 
-	assign rd[8*2-1:8] = RAM2[a[WIDTH*2-1:WIDTH]]; 
-	assign rd[8*3-1:8*2] = RAM3[a[WIDTH*3-1:WIDTH*2]]; 
-	assign rd[8*4-1:8*3] = RAM4[a[WIDTH*4-1:WIDTH*3]]; 
-	assign rd[8*5-1:8*4] = RAM5[a[WIDTH*5-1:WIDTH*4]]; 
-	assign rd[8*6-1:8*5] = RAM6[a[WIDTH*6-1:WIDTH*5]]; 
+	assign rd[WIDTH-1:0] = RAM1[a[WIDTH-1:0]]; 
+	assign rd[WIDTH*2-1:WIDTH] = RAM2[a[WIDTH*2-1:WIDTH]]; 
+	assign rd[WIDTH*3-1:WIDTH*2] = RAM3[a[WIDTH*3-1:WIDTH*2]]; 
+	assign rd[WIDTH*4-1:WIDTH*3] = RAM4[a[WIDTH*4-1:WIDTH*3]]; 
+	assign rd[WIDTH*5-1:WIDTH*4] = RAM5[a[WIDTH*5-1:WIDTH*4]]; 
+	assign rd[WIDTH*6-1:WIDTH*5] = RAM6[a[WIDTH*6-1:WIDTH*5]]; 
+	assign rd[WIDTH*7-1:WIDTH*6] = RAM7[a[WIDTH*7-1:WIDTH*6]]; 
 
 	always_ff @(posedge clk) begin
-		$display ($sformatf("write enable : %b, address: %h, newdata: %h", we, a[WIDTH*4-1:WIDTH*3], wd[WIDTH*4-1:WIDTH*3]));
 		if (we[0]) RAM1[a[WIDTH-1:0]] <= wd[WIDTH-1:0];
 		if (we[1]) RAM2[a[WIDTH*2-1:WIDTH]] <= wd[WIDTH*2-1:WIDTH];
 		if (we[2]) RAM3[a[WIDTH*3-1:WIDTH*2]] <= wd[WIDTH*3-1:WIDTH*2];
 		if (we[3]) RAM4[a[WIDTH*4-1:WIDTH*3]] <= wd[WIDTH*4-1:WIDTH*3];
 		if (we[4]) RAM5[a[WIDTH*5-1:WIDTH*4]] <= wd[WIDTH*5-1:WIDTH*4];
 		if (we[5]) RAM6[a[WIDTH*6-1:WIDTH*5]] <= wd[WIDTH*6-1:WIDTH*5];
+		if (we[6]) RAM7[a[WIDTH*7-1:WIDTH*6]] <= wd[WIDTH*7-1:WIDTH*6];
 	end
 endmodule
