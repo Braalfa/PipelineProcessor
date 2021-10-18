841be5b348473e3a1d9f14f0020dad0bce224523
Bratan <alfabrayan12@gmail.com>
2021-10-18 17:04:51 -0600

    generador de imagen, script de generacion de datos y gitignore

--- Python/hl.py
+++ Python/hl.py
@@ -3,7 +3,7 @@ import numpy as np
 # _________________________________/ALTO NIVEL/_________________________________
 LINE_SIZE = 41
 
-im = Image.new('1', (250, 250))
+im = Image.new('1', (300, 300), 'white')
 offsetX = 0
 offsetY = 0
 
@@ -21,15 +21,15 @@ for i in range(2500):
     bits = [int(c) for c in memmory[i]]
     bits.reverse()
     for j in range(25):
-        X = j % 5 + 5*(i%50)
-        Y = j//5 + 5*(i//50)
+        X = j % 5 + 6*(i%50)
+        Y = j//5 + 6*(i//50)
         if (bits[j] == 1):
             color = ImageColor.getcolor('black', '1')
         else:
             color = ImageColor.getcolor('white', '1')
         #print((X, Y), color, (offsetX, offsetY))
         im.putpixel((X, Y), color)
-
+     
 im.save('out.png')
 import matplotlib.pyplot as plt
 plt.imshow(im, cmap='gray', vmin=0,vmax=255)
